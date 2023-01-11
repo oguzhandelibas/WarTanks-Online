96381e458dbb446c77253f4aa1980c42152a988f
Oğuzhan Delibaş <oguzhan.delibass@gmail.com>
2023-01-11 15:44:05 +0300

    Control System + Material Assets
    -Added new material assets
    -Added new input & control systems

---
+++ Assets/Scripts/Input/InputData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter.PlayerInput
+{
+    [CreateAssetMenu(menuName = "Topdown Shooter/Input/Input Data")]
+    public class InputData : ScriptableObject
+    {
+        public float Horizontal;
+        public float Vertical;
+    }
+}

f339fd3f9970b3808d0513e911fbbaacd0f5e70b
Oğuzhan Delibaş <oguzhan.delibass@gmail.com>
2023-01-11 14:08:28 +0300

    Update CameraController.cs
    Edit basic movement on camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -13,6 +13,7 @@ namespace TopDownShooter
         private void Update()
         {
             CameraRotationFollow();
+            CameraMovementFollow();
         }
 
         private void CameraRotationFollow()
@@ -20,6 +21,12 @@ namespace TopDownShooter
             _cameraTransform.rotation = Quaternion.Lerp(_cameraTransform.rotation,
                 Quaternion.LookRotation(_targetTransform.position - _cameraTransform.position),
                 Time.deltaTime * _cameraSettings.RotationLerpSpeed);
+        }
+
+        private void CameraMovementFollow()
+        {
+            _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, _targetTransform.position
+                + _cameraSettings.PositionOffset, Time.deltaTime * _cameraSettings.PositionLerp);
         }
     }
 }
